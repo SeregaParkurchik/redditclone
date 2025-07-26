cb9e0be701bccc27ca2956949e7cd31371f316f9
SeregaParkurchik <serejjja3@yandex.ru>
2025-07-26 17:23:53 +0300

    добавил readme

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"redditclone/internal/core"
-	"redditclone/internal/handler"
-	"redditclone/internal/routes"
-	"redditclone/internal/storage"
+	"reddit_v2/internal/core"
+	"reddit_v2/internal/handlers"
+	"reddit_v2/internal/routes"
+	"reddit_v2/internal/storage"
 )
 
 func main() {
-	memStorage := storage.NewMemoryStorage()
-	authService := core.New(memStorage)
-	userHandler := handler.NewUserHandler(authService)
+
+	cfg := storage.PostgresConnConfig{
+		DBHost:   "host.docker.internal", //localhost
+		DBPort:   5432,
+		DBName:   "reddit",
+		Username: "reddit_admin",
+		Password: "qwerty",
+		Options:  nil, // или добавьте опции, если необходимо
+	}
+
+	// Создание соединения с базой данных
+	conn, err := storage.New(context.Background(), cfg)
+	if err != nil {
+		log.Fatalf("Не удалось подключиться к базе данных: %v", err)
+	}
+	defer conn.Close(context.Background())
+
+	// Создание экземпляра RedditDB
+	redditDB := storage.NewRedditDB(conn)
+
+	// Создание сервиса с использованием базы данных
+	authService := core.New(redditDB)
+	userHandler := handlers.NewUserHandler(authService)
 
 	mux := routes.InitRoutes(userHandler)
 	fmt.Println("Запуск сервера на порту 8080 http://localhost:8080/")
